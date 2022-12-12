58a734877462aae07ed201b025f07fb3011c62e0
Nathan Culley <nathan@nathanculley.com>
2022-12-12 16:55:15 -0500

    Rework displayLibrary to loop through properties of each object
    Rather than individually specifying a new row for each (specifically named) property, displayLibrary now simply loops through all the properties of each object (book) in myLibrary and adds each property as a new td in the same tr. The resulting code is much cleaner.

--- scripts.js
+++ scripts.js
@@ -5,18 +5,6 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        let readMessage;
-        if (this.read == false) {
-            readMessage = "not read yet.";
-        }
-        else {
-            readMessage = "already read.";
-        }
-    
-        let message = this.title + " " + "by " + this.author + ", " + pages + " pages, " + readMessage;
-        return message;
-    }
 }
 
 function addBook(title, author, pages, read) {
@@ -32,27 +20,14 @@ function displayLibrary(myLibrary) {
     const libraryTable = document.getElementById("library-table")
     for (let item of myLibrary) {
         const row = document.createElement("tr");
+        console.log(item);
+        for (let prop in item) {
+            console.log(item[prop]);
+            const node = document.createElement("td");
+            const textNode = document.createTextNode(item[prop]);
+            node.appendChild(textNode);
+            row.appendChild(node);
+        }
         libraryTable.appendChild(row);
-        const titleNode = document.createElement("td");
-        const titleText = document.createTextNode(item.title);
-        titleNode.appendChild(titleText);
-        console.log(item.title);
-        const authorNode = document.createElement("td");
-        const authorText = document.createTextNode(item.author);
-        authorNode.appendChild(authorText);
-        console.log(item.author);
-        const pagesNode = document.createElement("td");
-        const pagesText = document.createTextNode(item.pages);
-        pagesNode.appendChild(pagesText);
-        console.log(item.pages);
-        const readNode = document.createElement("td");
-        const readText = document.createTextNode(item.read);
-        readNode.appendChild(readText);
-        console.log(item.read);
-        row.appendChild(titleNode);
-        row.appendChild(authorNode);
-        row.appendChild(pagesNode);
-        row.appendChild(readNode);
-        
     }
 }
