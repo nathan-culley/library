a8be64da667d0cf0fe05aa139485c4efbbcfe621
Nathan Culley <nathan@nathanculley.com>
2022-12-12 18:19:04 -0500

    Remove displayLibrary function
    The displayLibrary() function has been commented out and will likely be removed entirely when I can be sure it won't break anything. There is now no initial display of books already hard-coded into the scripts.js file (The Hobbit and Dune), since those two books were only added as tests. Now the page just updates the table with new books as they're added.

--- scripts.js
+++ scripts.js
@@ -13,26 +13,26 @@ function addBook(title, author, pages, read) {
     console.table(myLibrary);
 }
 
-addBook("The Hobbit", "J.R.R. Tolkien", 250, true);
-addBook("Dune", "Frank Herbert", 400, true);
+// addBook("The Hobbit", "J.R.R. Tolkien", 250, true);
+// addBook("Dune", "Frank Herbert", 400, true);
 
-function displayLibrary(myLibrary) {
-    const libraryTable = document.getElementById("library-table")
-    for (let item of myLibrary) {
-        const row = document.createElement("tr");
-        console.log(item);
-        for (let prop in item) {
-            console.log(item[prop]);
-            const node = document.createElement("td");
-            const textNode = document.createTextNode(item[prop]);
-            node.appendChild(textNode);
-            row.appendChild(node);
-        }
-        libraryTable.appendChild(row);
-    }
-}
+// function displayLibrary(myLibrary) {
+//     const libraryTable = document.getElementById("library-table")
+//     for (let item of myLibrary) {
+//         const row = document.createElement("tr");
+//         console.log(item);
+//         for (let prop in item) {
+//             console.log(item[prop]);
+//             const node = document.createElement("td");
+//             const textNode = document.createTextNode(item[prop]);
+//             node.appendChild(textNode);
+//             row.appendChild(node);
+//         }
+//         libraryTable.appendChild(row);
+//     }
+// }
 
-displayLibrary(myLibrary);
+// displayLibrary(myLibrary);
 
 function displayNewBook(myLibrary) {
     const libraryTable = document.getElementById("library-table")
