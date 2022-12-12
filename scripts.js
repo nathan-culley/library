3d545e6e455e2bae41f35966360d4c12360b7043
Nathan Culley <nathan@nathanculley.com>
2022-12-12 15:07:24 -0500

    Create Book() constructor
    Created a constructor function to add new books as objects, eventually to be added to the myLibrary array.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBooktoLibrary() {
+
+}
