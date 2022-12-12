9b7202a2634692f623bc9dc61ec58595c7016a76
Nathan Culley <nathan@nathanculley.com>
2022-12-12 15:25:49 -0500

    Added info function to the constructor
    Added an info function to the Book constructor that returns a sentence with a brief summary of the book's info.

--- scripts.js
+++ scripts.js
@@ -5,8 +5,33 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
     this.read = false;
+    this.info = function() {
+        let readMessage;
+        if (this.read == false) {
+            readMessage = "not read yet.";
+        }
+        else {
+            readMessage = "already read.";
+        }
+    
+        let message = this.title + " " + "by " + this.author + ", " + pages + " pages, " + readMessage;
+        return message;
+    }
 }
 
-function addBooktoLibrary() {
+/* function info(bookTitle) {
+    if (bookTitle.read == false) {
+        let readMessage = "not read yet.";
+    }
+    else {
+        let readMessage = "already read.";
+    }
 
+    let message = bookTitle.title + " " + "by " + bookTitle.author + ", " + pages + " pages, " + readMessage;
+    return message;
+} */
+
+function addBooktoLibrary(newBook, title, author, pages) {
+    newBook = new Book(title, author, pages);
+    console.log(newBook);    
 }
